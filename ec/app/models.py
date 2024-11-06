f26587640d311a1bd4f76bb8e2a7a6ca1588b1c7
Neski <eneskadiri10@gmail.com>
2024-11-06 23:29:22 +0100

    login

--- ec/app/models.py
+++ ec/app/models.py
@@ -1,6 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+STATE_CHOICES = (
+    ('Andaman & Nicobar Islands','Andaman & Nicovar Islands'),
+    ('Andhra Pradesh','Andhra Pradesh'),
+    ('Arunachal Pradesh','Aranachal Pradesh'),
+    ('Assam','Assam'),
+    ('Bihar','Bihar'),
+    ('Chandigarh','Chandigarh'),
+    ('Dadra & Nagar Haveli','Dadra & Nagar Haveli'),
+    ('Daman and Diu','Daman and Diu'),
+    ('Delhi','Delhi'),
+    ('Goa','Goa'),
+    ('Gujrat','Gujrat'),
+    ('Haryana','Haryana'),
+    ('Himachal Pradesh','Delhi'),
+    ('Jammu & Kashmir','Jammu & Kashmir'),
+    ('Jharkhand','Jharkhand'),
+    ('Karnataka','Karnataka'),
+    ('Kerala','Kerala'),
+    ('Lakshadweep','Lakshadweep'),
+    ('Madhya Pradesh','Madhya Pradesh'),
+    ('Maharashtra','Maharashtra'),
+    ('Manipur','Manipur'),
+    ('Meghalaya','Meghalaya'),
+    ('Mizoram','Mizoram'),
+    ('Nagaland','Nagaland'),
+    ('Odisa','Odisa'),
+    ('Poducherry','Poducherry'),
+    ('Punjab','Punjab'),
+    ('Rajasthan','Rajasthan'),
+    ('Sikkim','Sikkim'),
+    ('Tamil Nadu','Tamil Nadu'),
+    ('Telangana','Telangana'),
+    ('Tripura','Tripura'),
+    ('Uttarakhand','Uttarakhand'),
+    ('Uttar Pradesh','Uttar Pradesh'),
+    ('West Bengal','West Bengal'),
+)
 
 CATEGORY_CHOICES = (
     ('CR','Crud'),
@@ -24,3 +62,14 @@ class Product(models.Model):
     product_image = models.ImageField(upload_to='product')
     def _str_(self):
         return self.title
+    
+class Customer(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES,max_length=100)
+    def __str__(self):
+        return self.name
